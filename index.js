4d9b28e9879835c41a8f2684aa1f23cb34df6f0a
Snuffz <51891016+Snuffz@users.noreply.github.com>
2020-07-18 20:25:28 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
+const client = new Discord.Client();
+const config = require("./config.json");
+client.config = config;
+
+// handler:
+fs.readdir("./eventos/", (err, files) => {
+  if (err) return console.error(err); // retorna no console caso ocorra algum erro
+  files.forEach(file => {
+    const event = require(`./events/${file}`); // pega o arquivo que está na pasta eventos
+    let eventName = file.split(".")[0]; 
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err); // retorna no console caso ocorra algum erro
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./comandos/${file}`); // pega o arquivo que está na pasta comandos
+    let commandName = file.split(".")[0];
+    console.log(`${commandName} carregado.`);
+    client.commands.set(commandName, props);
+  });
+});
+
+// iniciar o bot:
+client.login(config.token);
